c93e4980cc2a4e96f14e92c2a883f830edc737e2
DanielAmah <amahdanieljack@gmail.com>
2018-09-01 09:00:50 +0100

    implement unique file upload

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -1,4 +1,11 @@
 class FileUpload < ApplicationRecord
   mount_uploader :attachment, AttachmentUploader
   validates :name, presence: true
+
+  def save
+    super
+    rescue ActiveRecord::RecordNotUnique => err
+      errors.add :attachment, "'#{attachment.file.original_filename}' already exists"
+      false
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_222532) do
+ActiveRecord::Schema.define(version: 2018_08_30_004252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_08_28_222532) do
     t.string "attachment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["attachment"], name: "index_file_uploads_on_attachment", unique: true
   end
 
 end
